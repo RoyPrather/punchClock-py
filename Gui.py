9e83e7b6f748b80103cdb70362fda245b4ecd829
Roy Prather <royprather@icloud.com>
2018-03-13 16:57:33 -0700

    removed gpio cleanups

--- Gui.py
+++ Gui.py
@@ -4,6 +4,9 @@ import RPi.GPIO as GPIO
 import MFRC522
 from Database import *
 
+#bug fixes
+GPIO.setwarnings(False)
+
 # style args
 font = 'verdana 15 bold'
 
@@ -44,7 +47,6 @@ class ScanLabel(MyLabel) :
                 if status == self.reader.MI_OK :
                     uid = str(uid[0]) + str(uid[1]) + str(uid[2]) + str(uid[3])
                     employee.newEmployee('admin' , uid)
-                    GPIO.cleanup()
                 else :
                     self.label.configure(text = 'Please Scan New Admin Card' , bg = 'red')
                     createAdmin()
@@ -69,16 +71,13 @@ class ScanLabel(MyLabel) :
                     emp = employee(self.uid)
                     if emp.name == 'admin':
                         self.label.config(text = "Admin Card Read" , bg = 'green')
-                        GPIO.cleanup()
                         self.label.bind('<1>' , lambda x : self.adminfunc())
                         self.after(5000 , self.tick)
                     else:
                         self.label.config(text = emp.name , bg = 'green')
-                        GPIO.cleanup()
                         self.label.bind('<1>' , lambda x: self.function(self.uid))
                         self.after(5000, self.tick)
                 except:
-                    GPIO.cleanup()
                     self.tick()
         else:
             self.after(300 , self.tick)
