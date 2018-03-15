200d5cb9933d4306e200b2dc64a97c1ea2f5703f
Roy Prather <royprather@icloud.com>
2018-03-15 15:49:41 -0700

    testing list box output

--- TimeClock.py
+++ TimeClock.py
@@ -320,7 +320,7 @@ def showLog() :
 
     #bind widgets
     backButton.label.bind('<1>' , lambda x: t.destroy())
-    submitButton.label.bind('<1>' , lambda x: timeCard(emps[nameFrame.curselection()]))
+    submitButton.label.bind('<1>' , lambda x: timeCard(emps[nameFrame.curselection()[0]]))
 
 def timeCard(emp) :
     # create window
