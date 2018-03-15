85b3481c0967a104694849ba117e8d35cc6b961f
Roy Prather <royprather@icloud.com>
2018-03-15 15:17:43 -0700

    adjusting listbox

--- TimeClock.py
+++ TimeClock.py
@@ -290,7 +290,7 @@ def showLog() :
     canvasBox = Tk.Frame(t, width = setWidth(85) , height = setHeight(70))
     backButton = BlueButton(t , width = setWidth(25) , height = setHeight(15))
     scrollBar = MyScrollBar(canvasBox , width = setWidth(10) , height = setHeight(70))
-    nameFrame = Tk.Listbox(canvasBox , width = setWidth(75) , height = setHeight(70) , yscrollcommand = scrollBar.scrollBar.set , selectmode ='single')
+    nameFrame = Tk.Listbox(canvasBox , width = setWidth(75) , height = setHeight(70) , yscrollcommand = scrollBar.scrollBar.set , selectmode ='single' , font = font)
 
     #configure widgets
     titleLabel.label.configure(text = 'Choose and Employee to Veiw')
