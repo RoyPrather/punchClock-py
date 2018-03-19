20d8345771fd19eb7954f760006783499f546315
Roy Prather <royprather@icloud.com>
2018-03-19 15:40:37 -0700

    bugfix

--- TimeClock.py
+++ TimeClock.py
@@ -313,7 +313,7 @@ def timeCardList() :
         emp = employee(uid[0])
         if emp.name != 'admin':
             emps.insert(count , emp)
-            nameFrame.insert(count , emp.name + '    ' + emp.hours / 360  + '  Hours')
+            nameFrame.insert(count , emp.name + '    ' + str(emp.hours / 360.0)  + '  Hours')
             count += 1
 
     #bind widgets
